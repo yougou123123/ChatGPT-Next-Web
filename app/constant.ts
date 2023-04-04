a67e2d8d77dd772e4718e7a0cadbaf2aa961d4f3
yougou123123 <129817831+yougou123123@users.noreply.github.com>
2023-04-04 13:46:23 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -1,7 +1,7 @@
 export const OWNER = "Yidadaa";
-export const REPO = "ChatGPT-Next-Web";
-export const REPO_URL = `https://github.com/${OWNER}/${REPO}`;
-export const ISSUE_URL = `https://github.com/${OWNER}/${REPO}/issues`;
-export const UPDATE_URL = `${REPO_URL}#%E4%BF%9D%E6%8C%81%E6%9B%B4%E6%96%B0-keep-updated`;
-export const FETCH_COMMIT_URL = `https://api.github.com/repos/${OWNER}/${REPO}/commits?per_page=1`;
-export const FETCH_TAG_URL = `https://api.github.com/repos/${OWNER}/${REPO}/tags?per_page=1`;
+export const REPO = "ChatGPT-Web";
+export const REPO_URL = `https://wpa.qq.com/msgrd?v=1&uin=860325252`;
+export const ISSUE_URL = `https://wpa.qq.com/msgrd?v=1&uin=860325252`;
+export const UPDATE_URL = `/`;
+export const FETCH_COMMIT_URL = `/`;
+export const FETCH_TAG_URL = `/`;
